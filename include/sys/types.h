bbd8478178790155f7ded3e99a5441c4d270e32b
Masami Ichikawa <masami256@gmail.com>
2013-07-01 23:09:44 +0900

    Add sys/types.h
    Having some special types are useful such as u_int32_t, uchar, and so
    on.

---
+++ include/sys/types.h
@@ -0,0 +1,20 @@
+#ifndef __MIKOTO_SYS_TYPES_H
+#define __MIKOTO_SYS_TYPES_H
+
+typedef unsigned int size_t;
+typedef int ssize_t;
+
+typedef char int8_t;
+typedef unsigned char u_int8_t;
+typedef short int16_t;
+typedef unsigned short u_int16_t;
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+typedef unsigned char uchar;
+typedef unsigned short ushort;
+typedef unsigned int uint;
+typedef unsigned long ulong;
+
+#endif // __MIKOTO_SYS_TYPES_H
+
