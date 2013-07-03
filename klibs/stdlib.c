60d47dafcd89a60592d4bfadb970e6d89922957b
Masami Ichikawa <masami256@gmail.com>
2013-07-03 23:25:14 +0900

    fix indent.

--- klibs/stdlib.c
+++ klibs/stdlib.c
@@ -9,24 +9,24 @@ void
 itoa(long n, char *buf)
 {
 	int i, len, sign;
-        char tmp[BUF_SIZE] = { 0 };
-        i = len = sign = 0;
+	char tmp[BUF_SIZE] = { 0 };
+	i = len = sign = 0;
 
-        if (n < 0) {
+	if (n < 0) {
 		sign = 1;
-                n = -n;
-        }
+		n = -n;
+	}
 
-        do {
-                tmp[len++] = n % 10 + '0';
+	do {
+		tmp[len++] = n % 10 + '0';
 	}  while ((n /= 10) > 0 && len < BUF_SIZE - 2);
 
 	if (sign)
-                tmp[len++] = '-';
+		tmp[len++] = '-';
 
 	for (i = 0; i < len; i++)
-                buf[i] = tmp[len - 1 - i];
-        buf[i] = 0x0;
+		buf[i] = tmp[len - 1 - i];
+	buf[i] = 0x0;
 }
 
 /**
@@ -37,20 +37,20 @@ itoa(long n, char *buf)
 void 
 itox(unsigned long n, char *buf)
 {
-        int i, len;
-        char tmp[BUF_SIZE] = { 0 };
-        i = len = 0;
-        static const char hex[] = {
-                '0', '1', '2', '3', '4', '5',
-                '6', '7', '8', '9', 'a', 'b',
-                'c', 'd', 'e', 'f' };
+	int i, len;
+	char tmp[BUF_SIZE] = { 0 };
+	i = len = 0;
+	static const char hex[] = {
+		'0', '1', '2', '3', '4', '5',
+		'6', '7', '8', '9', 'a', 'b',
+		'c', 'd', 'e', 'f' };
 
-        do {
-                tmp[len++] = hex[n % 16];
-        }  while ((n /= 16) > 0 && len < BUF_SIZE - 1);
+	do {
+		tmp[len++] = hex[n % 16];
+	}  while ((n /= 16) > 0 && len < BUF_SIZE - 1);
 
-        for (i = 0; i < len; i++)
-                buf[i] = tmp[len - 1 - i];
-        buf[i] = 0x0;
+	for (i = 0; i < len; i++)
+		buf[i] = tmp[len - 1 - i];
+	buf[i] = 0x0;
 }
 
