19b92cd28cce957bb09a1c5fd76dd28fb28f26d5
Masami Ichikawa <masami256@gmail.com>
2016-04-21 00:06:44 +0900

    Add ascii art.

--- init/kernel_main.c
+++ init/kernel_main.c
@@ -2,16 +2,26 @@
 #include <mikoto/printf.h>
 #include <mikoto/mm.h>
 
-void
-kernel_main(void)
+char msg[] = 
+"                                   _       _   _                        _ \n"
+"                                  (_)     (_) | |                      | |\n"
+"   __ _ _ __ _ __ ___    _ __ ___  _ _ __  _  | | _____ _ __ _ __   ___| |\n"
+"  / _` | '__| '_ ` _ \\  | '_ ` _ \\| | '_ \\| | | |/ / _ \\ '__| '_ \\ / _ \\ |\n"
+" | (_| | |  | | | | | | | | | | | | | | | | | |   <  __/ |  | | | |  __/ |\n"
+"  \\__,_|_|  |_| |_| |_| |_| |_| |_|_|_| |_|_| |_|\\_\\___|_|  |_| |_|\\___|_|\n"
+"                                                                          \n"
+"                                                                          \n";
+
+void kernel_main(void)
 {
 	char s[256] = { 0 };
 	// setup paging
-	paging_init();
+	//paging_init();
 
 	sprintf(s, "%s\n", "hello, world!");
+	uart_puts(msg);
+	uart_puts("\n");
 	uart_puts(s);
-
 	while (1);
 }
 
